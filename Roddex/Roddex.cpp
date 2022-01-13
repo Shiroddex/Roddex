fb2925b523d562c417173805f325891bc12ae674
Носов Егор <IMXO3NEXUS@gmail.com>
2022-01-13 14:45:34 +0300

    Добавьте файлы проекта.

---
+++ Roddex/Roddex.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    float Far, Cel;
+    cout << "Far = "; cin >> Far;
+    Cel = (Far - 32) * 5 / 9;
+    cout << "Cel = " << Cel << endl;
+    cout << "Cel = "; cin >> Cel;
+    Far = Cel * 9 / 5 + 32;
+    cout << "Far = " << Far << endl;
+    return 0;
+}
